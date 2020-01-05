b1b18fbeddd3dff2dca533d5fa1abbc14b9bc18a
Your Name <jamesmeier94@gmail.com>
2020-01-05 01:43:47 -0500

    D

---
+++ client/build/precache-manifest.5ba41bb1123639a0c3f6b9ea5c34af27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "11b7cf894ab191355612",
+    "url": "/static/css/main.85f083b6.chunk.css"
+  },
+  {
+    "revision": "11b7cf894ab191355612",
+    "url": "/static/js/main.7fcd53b4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "645f027fd53b988143f5",
+    "url": "/static/js/2.6aa799ff.chunk.js"
+  },
+  {
+    "revision": "74e82eaf78ead0b2fef1300d38fc0da5",
+    "url": "/index.html"
+  }
+];
